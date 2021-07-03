c4de5bb4cb16d8f5a23ab1fc6fa995cc9271f9d4
caiomcs04 <caio6c@yahoo.com.br>
2021-07-03 12:23:10 -0300

    8#Commit(Ajust)

--- personalapi/src/main/java/one/dio/personalapi/repository/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package one.dio.personalapi.repository;
-
-import one.dio.personalapi.entity.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Long> {
-}
--- personalapi/src/main/java/one/dio/personalapi/repository/PhoneRepository.java
+++
@@ -1,7 +0,0 @@
-package one.dio.personalapi.repository;
-
-import one.dio.personalapi.entity.Phone;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PhoneRepository extends JpaRepository<Phone, Long> {
-}
